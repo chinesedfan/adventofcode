1727d1fac4cd8bff69dded5f4905de904d940fdc
chinesedfan <chinesedfan@qq.com>
2017-09-20 18:09:11 +0800

    support -f

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var year = process.env.YEAR || '2015';
 var day, part, solve;
 
 if (args.length < 1) {
-	console.log('Useage: node ' + path.basename(__filename) + ' <day> [part]');
+	console.log('Useage: node ' + path.basename(__filename) + ' <day> [part] [-f input]');
 	return;
 }
 
@@ -15,11 +15,18 @@ if (day.length < 2) {
     day = '0' + day;
 }
 part = 1;
-if (args.length > 0) {
+if (args.length > 0 && /^1|2$/.test(args[0])) {
 	part = args.shift();
 }
 
 solve = require('./lib/' + year + '/day' + day);
+if (args.length > 1 && args[0] === '-f') {
+	fs.readFile(args[1], (err, buf) => {
+		if (err) throw err;
+		solve(part, buf.toString());
+	});
+	return;
+}
 process.stdin.on('data', function(chunk) {
 	var data = chunk.toString();
 	if (data === '\n') {
